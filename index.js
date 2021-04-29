dab09264bb4a297b4fc812c0ed12ecae335b24d9
RenatoExpert <renatorraraujo@gmail.com>
2021-04-29 04:32:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const app = express();
-const port = 8080;
+//const port = 8080;
+const port process.envPORT;
 const path = require ('path');
 app.use(express.static(path.join(__dirname,'public')));
 
