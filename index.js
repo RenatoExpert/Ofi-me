2c421c7bd960f3ebf5a70f53388ea4cf5b030da1
RenatoExpert <renatorraraujo@gmail.com>
2021-04-29 04:51:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require ('path');
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', (req,res) => {
-	res.fileSend('/index.html')
+	res.sendFile('/index.html')
 });
 
 app.listen (port, () => {
